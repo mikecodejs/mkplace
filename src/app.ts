ea7b3e28c34ec2a48e9babcd2c203f27daec51bc
Mike Figueiredo <mikerxvxl@gmail.com>
2022-03-07 19:50:47 -0400

    feat: :star: server e prisma

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
 import express, { Application } from "express";
 import morgan from "morgan";
 
+import "express-async-errors";
+
+import { router } from "./core/routes";
+import { corsMiddleware } from "./core/cors";
+import { errorMiddleware } from "./core/error";
+
 const app: Application = express();
+
+app.use(corsMiddleware);
 
 app.use(morgan("dev"));
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", router);
+
+app.use(errorMiddleware);
 
 const port = 8080;
 
