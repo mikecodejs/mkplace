853d4e879a318e18ae92579c246583eee6912733
Maykel Figueiredo <mikerxvxl@gmail.com>
2022-03-08 00:53:56 -0400

    feat: :star: adiciona controllers do módulo sale

---
+++ src/modules/controllers/sale/saleCreateController.ts
@@ -0,0 +1,15 @@
+import { User } from "@prisma/client";
+import { Request, Response } from "express";
+import { saleCreateResolver } from "../../resolvers/sale/saleCreateResolver";
+
+export const saleCreateController = async (
+	request: Request,
+	response: Response,
+): Promise<Response<User>> => {
+	const sale = await saleCreateResolver(request.body);
+
+	return response.status(201).json({
+		message: "✨ sale successfully created ",
+		sale,
+	});
+};
