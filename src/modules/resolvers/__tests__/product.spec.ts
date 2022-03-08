0f165c55c0a54db3ce6e82e5b3bb2efca8cee98d
Maykel Figueiredo <mikerxvxl@gmail.com>
2022-03-08 00:35:16 -0400

    test: :vertical_traffic_light: adiciona testes para módulo de produtos

---
+++ src/modules/resolvers/__tests__/product.spec.ts
@@ -0,0 +1,74 @@
+import { prismaMock } from "../../../singleton";
+
+import { productCreateResolver } from "../product/productCreateResolver";
+import { productDeleteResolver } from "../product/productDeleteResolver";
+import { productGetAllResolver } from "../product/productGetAllResolver";
+import { productUpdateResolver } from "../product/productUpdateResolver";
+
+const mockDate = new Date();
+
+const products = [
+	{
+		id: "622019b3750df0577581017f",
+		name: "Samsung Galaxy A13",
+		brand: "Samsung",
+		price: 2032.5,
+		createdAt: mockDate,
+		updatedAt: mockDate,
+	},
+	{
+		id: "6220226bb46ecd165a731f87",
+		name: "TV 60p 4K FULL HD Samsung",
+		brand: "Samsung",
+		price: 3489,
+		createdAt: mockDate,
+		updatedAt: mockDate,
+	},
+];
+
+it("should create new product", () => {
+	prismaMock.product.create.mockResolvedValue(products[0]);
+
+	const test = productCreateResolver(products[0]);
+
+	expect(test).resolves.toBeTruthy();
+	expect(test).resolves.toEqual(products[0]);
+});
+
+it("should return all products", () => {
+	prismaMock.product.findMany.mockResolvedValue(products);
+
+	const test = productGetAllResolver();
+
+	expect(test).resolves.toHaveLength(2);
+	expect(test).resolves.toEqual(products);
+});
+
+it("should return an error when not finding products", () => {
+	prismaMock.product.findMany.mockRejectedValue(
+		new Error("Not a found products in database"),
+	);
+
+	const test = productGetAllResolver();
+
+	expect(test).rejects.toEqual(new Error("Not a found products in database"));
+});
+
+it("should be able to update a product", () => {
+	prismaMock.product.findUnique.mockResolvedValue(products[0]);
+	prismaMock.product.update.mockResolvedValue(products[0]);
+
+	const test = productUpdateResolver(products[0].id, products[0]);
+
+	expect(test).resolves.toBeTruthy();
+	expect(test).resolves.toEqual(products[0]);
+});
+
+it("should be able to delete a product", () => {
+	prismaMock.product.findUnique.mockResolvedValue(products[0]);
+	prismaMock.product.delete.mockResolvedValue(products[0]);
+
+	const test = productDeleteResolver(products[0].id);
+
+	expect(test).resolves.toBeTruthy();
+});
