566941786fdd3b6fcc6e086c765173ff169fb98c
Maykel Figueiredo <mikerxvxl@gmail.com>
2022-03-08 00:53:03 -0400

    feat: :star: adiciona resolver de sale

---
+++ src/modules/resolvers/sale/saleCreateResolver.ts
@@ -0,0 +1,24 @@
+import { Sale } from "@prisma/client";
+import prisma from "../../../client";
+
+export const saleCreateResolver = async (sale: Sale): Promise<Sale> => {
+	const isUser = await prisma.user.findUnique({ where: { id: sale.userId } });
+	const isProduct = await prisma.user.findUnique({
+		where: { id: sale.productId },
+	});
+
+	if (!isUser) {
+		throw new Error("user does not exists!");
+	}
+
+	if (!isProduct) {
+		throw new Error("product does not exists!");
+	}
+
+	return await prisma.sale.create({
+		data: {
+			userId: sale.userId,
+			productId: sale.productId,
+		},
+	});
+};
