12d6e2312b66cdb126702f9aeb8ea994c3bd8bab
Maykel Figueiredo <mikerxvxl@gmail.com>
2022-03-08 00:55:18 -0400

    fix: :beetle: corrige rotas de produtos e adiciona rota de sale

--- src/core/routes.ts
+++ src/core/routes.ts
@@ -6,6 +6,10 @@ import { userGetAllController } from "../modules/controllers/user/userGetAllCont
 import { userUpdateController } from "../modules/controllers/user/userUpdateController";
 
 import { productCreateController } from "../modules/controllers/product/productCreateController";
+import { saleCreateController } from "../modules/controllers/sale/saleCreateController";
+import { productUpdateController } from "../modules/controllers/product/productUpdateController";
+import { productDeleteController } from "../modules/controllers/product/productDeleteController";
+import { productGetAllController } from "../modules/controllers/product/productGetAllController";
 
 const router = Router();
 
@@ -14,6 +18,11 @@ router.post("/user", userCreateController);
 router.put("/user/:id", userUpdateController);
 router.delete("/user/:id", userDeleteController);
 
+router.get("/product", productGetAllController);
 router.post("/product", productCreateController);
+router.put("/product", productUpdateController);
+router.delete("/product", productDeleteController);
+
+router.post("/sale", saleCreateController);
 
 export { router };
