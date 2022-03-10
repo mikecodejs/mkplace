a82a61ee65fdab5fff36bc3c13e4960455443832
Mike Figueiredo <mikerxvxl@gmail.com>
2022-03-10 00:22:52 -0400

    test: :vertical_traffic_light: covarage 100% saleCreateResolver

--- src/modules/resolvers/__tests__/sale.spec.ts
+++ src/modules/resolvers/__tests__/sale.spec.ts
@@ -33,11 +33,30 @@ const sale = {
 it("should create new user", () => {
 	prismaMock.user.findUnique.mockResolvedValue(user);
 	prismaMock.product.findUnique.mockResolvedValue(product);
-
 	prismaMock.sale.create.mockResolvedValue(sale);
 
 	const test = saleCreateResolver(sale);
 
 	expect(test).resolves.toBeTruthy();
 	expect(test).resolves.toEqual(sale);
+});
+
+it("should not be able if user does not exists", () => {
+	prismaMock.product.findUnique.mockResolvedValue(product);
+	prismaMock.sale.create.mockRejectedValue(new Error("user does not exists!"));
+
+	const test = saleCreateResolver(sale);
+
+	expect(test).rejects.toEqual(new Error("user does not exists!"));
+});
+
+it("should not be able if product does not exists", () => {
+	prismaMock.user.findUnique.mockResolvedValue(user);
+	prismaMock.sale.create.mockRejectedValue(
+		new Error("product does not exists!"),
+	);
+
+	const test = saleCreateResolver(sale);
+
+	expect(test).rejects.toEqual(new Error("product does not exists!"));
 });
