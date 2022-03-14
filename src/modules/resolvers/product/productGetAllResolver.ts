50fb36b273cea94bdb50b118f28563057cd97ce5
Maykel Figueiredo <mikerxvxl@gmail.com>
2022-03-13 20:23:18 -0400

    feat: :star: adiciona método para listar todos os usuários

--- src/modules/resolvers/product/productGetAllResolver.ts
+++ src/modules/resolvers/product/productGetAllResolver.ts
@@ -2,7 +2,11 @@ import { Product } from "@prisma/client";
 import prisma from "../../../client";
 
 export const productGetAllResolver = async (): Promise<Product[]> => {
-	const products = await prisma.product.findMany();
+	const products = await prisma.product.findMany({
+		include: {
+			sale: true,
+		},
+	});
 
 	if (!products || products.length === 0) {
 		throw new Error("Not a found products in database");
